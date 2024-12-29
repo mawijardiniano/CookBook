55aba4d778182140eaf902c1d20bf092ed6df0d9
Mawi <jardinianomarklouisse@gmail.com>
2024-12-29 13:42:18 +0800

    sort recipes on home and profile page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, memo } from "react";
 import axios from "axios";
 import { Input } from "@/components/ui/input";
 import { FaBell, FaBookmark, FaComment, FaHeart, FaShare } from "react-icons/fa";
@@ -6,8 +6,8 @@ import "../App.css";
 export default function Home() {
   const [query, setQuery] = useState("");
   const [account] = useState("Mawi");
-  const [recipes, setRecipes] = useState([]);
-  const time = "2 Hours";
+  const [recipe, setRecipes] = useState([]);
+    const [date, setDate] = useState(new Date());
 
   const RecipeAPI = "http://localhost:5000/api/recipe/create-recipe";
   const GetRecipeAPI = "http://localhost:5000/api/recipe/get-recipe";
@@ -24,54 +24,60 @@ export default function Home() {
   useEffect(() => {
     getRecipes();
   }, []);
+  
+  const MemoizedRecipe = memo (({recipes}) => (
+    <div className="mt-4">
+    {recipes.length > 0 ? (
+      recipes.map((recipe) => (
+        <div
+          key={recipe._id}
+          className="border py-2 px-4 mb-4 rounded-[6px] bg-white"
+        >
+          <div className="flex flex-row space-x-2 items-center">
+            <div className="p-6 rounded-full bg-gray-200" />
+            <div className="flex flex-row items-center justify-between w-full">
+            <div>
+            <p className="text-sm font-medium">{recipe.createdBy?.name || "Null"}</p>
+            <p className="text-[12px]">{recipe.timeSince}</p>
+            </div>
+            <div>
+            <FaBookmark/>
+            </div>
 
+            </div>
+          </div>
+          <h3 className="text-xl font-semibold">{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <div className="flex flex-row justify-between px-20 pt-2 border-t-2 border-gray-200">
+            <div className="flex flex-row space-x-2 items-center">
+              <FaHeart size={20} />
+              <p className="text-sm">Like</p>
+            </div>
+            <div className="flex flex-row space-x-2 items-center">
+              <FaComment size={20} />
+              <p className="text-sm">Comment</p>
+            </div>
+
+            <div className="flex flex-row space-x-2 items-center">
+              <FaShare size={20} />
+              <p className="text-sm">Share</p>
+            </div>
+          </div>
+        </div>
+      ))
+    ) : (
+      <p>No recipes found.</p>
+    )}
+    </div>
+  ));
+
+  const sortedRecipes = [...recipe]
+    .sort((a, b) => new Date(b.createdOn) - new Date(a.createdOn));
 
   return (
     <div className="flex-1 flex flex-col w-full py-20 px-10">
       <div className="px-6">
-        <div className="mt-4">
-          {recipes.length > 0 ? (
-            recipes.map((recipe) => (
-              <div
-                key={recipe._id}
-                className="border py-2 px-4 mb-4 rounded-[6px] bg-white"
-              >
-                <div className="flex flex-row space-x-2 items-center">
-                  <div className="p-6 rounded-full bg-gray-200" />
-                  <div className="flex flex-row items-center justify-between w-full">
-                  <div>
-                  <p className="text-sm font-medium">{recipe.createdBy?.name || "Null"}</p>
-                  <p className="text-[12px]">{recipe.timeSince}</p>
-                  </div>
-                  <div>
-                  <FaBookmark/>
-                  </div>
-
-                  </div>
-                </div>
-                <h3 className="text-xl font-semibold">{recipe.title}</h3>
-                <p>{recipe.description}</p>
-                <div className="flex flex-row justify-between px-20 pt-2 border-t-2 border-gray-200">
-                  <div className="flex flex-row space-x-2 items-center">
-                    <FaHeart size={20} />
-                    <p className="text-sm">Like</p>
-                  </div>
-                  <div className="flex flex-row space-x-2 items-center">
-                    <FaComment size={20} />
-                    <p className="text-sm">Comment</p>
-                  </div>
-
-                  <div className="flex flex-row space-x-2 items-center">
-                    <FaShare size={20} />
-                    <p className="text-sm">Share</p>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>No recipes found.</p>
-          )}
-        </div>
+<MemoizedRecipe recipes={sortedRecipes}/>
       </div>
     </div>
   );
